cc31253d5a0f5e02c808f4b4376b971503099940
AshV <ashish6318@gmail.com>
2017-11-29 01:36:16 +0530

    Understanding Image and BitMap types

--- ImageProcessing/Program.cs
+++ ImageProcessing/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
 
 namespace ImageProcessing
 {
@@ -10,6 +13,16 @@ namespace ImageProcessing
     {
         static void Main(string[] args)
         {
+            var a = Bitmap.FromFile(@"F:\Downloads\WhatsApp Image 2017-11-27 at 21.23.15.jpeg");
+            WriteLine(a.Height);
+            WriteLine(a.Width);
+            WriteLine(a.HorizontalResolution);
+            WriteLine(a.VerticalResolution);
+            var b = new Bitmap(a);
+            WriteLine(b.Height);
+            WriteLine(b.Width);
+            WriteLine(b.HorizontalResolution);
+            WriteLine(b.VerticalResolution);
         }
     }
 }
